072da335086c574ea0053b9dbba1a9b7fbb00283
adrashmadra <dulustan77@gmail.com>
2020-11-12 16:17:52 +0900

    Sreda

---
+++ app/src/main/java/com/example/multimpleacrivitypetrov/Illusion.java
@@ -0,0 +1,14 @@
+package com.example.multimpleacrivitypetrov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Illusion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_illusion);
+    }
+}
